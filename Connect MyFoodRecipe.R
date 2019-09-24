abd8b5f7998610467aa537a74656fa1fcff0e05e
b3708-101 <55365013+b3708-101@users.noreply.github.com>
2019-09-24 09:04:29 -1000

    Delete Connect MyFoodRecipe.R

--- Connect MyFoodRecipe.R
+++
@@ -1,6 +0,0 @@
-#Connect to MyRecipe Database
-drv <- dbDriver("PostgreSQL")
-con <- dbConnect(drv, dbname= 'MyFoodRecipe',host = "localhost", port = 5432,
-                                 user = "postgres", password = 'FoodData')
-drv <- dbDriver("MySQL")
-con<- dbConnect(drv, dbname='mysql', host= "127.0.0.1", port=5431, user='brandonlum',password = 'FoodData1')
